3fb2e4c8815824d931b31f88f0f84c5c2dd65e6c
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2020-09-27 21:36:44 +0545

    add background in main activity

--- app/src/main/java/com/alisha/takeawayapp/tabs/Tab1Fragment.java
+++ app/src/main/java/com/alisha/takeawayapp/tabs/Tab1Fragment.java
@@ -1,16 +1,32 @@
 package com.alisha.takeawayapp.tabs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.alisha.takeawayapp.MainActivity;
 import com.alisha.takeawayapp.R;
 
-public class Tab1Fragment extends Fragment {@Override
-public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.activity_login, container, false);
-}
+public class Tab1Fragment extends Fragment {
+Button btnSignIn;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.activity_login, container, false);
+        btnSignIn = view.findViewById(R.id.btn_signIN);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), MainActivity.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+        return view;
+    }
 }
