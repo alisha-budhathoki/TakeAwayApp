7607a1bacad5b5a2aab71bfda64c4dfcfaf82825
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2020-10-03 17:33:59 +0545

    adding frgaments in bottom navigation

--- app/src/main/java/com/alisha/takeawayapp/MainActivity.java
+++ app/src/main/java/com/alisha/takeawayapp/MainActivity.java
@@ -1,14 +1,49 @@
 package com.alisha.takeawayapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.alisha.takeawayapp.bottomNavItems.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.base_main);
+        bottomNavigation = findViewById(R.id.navigation);
+        BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.navigation_home:
+                                openFragment(new HomeFragment());
+                                return true;
+                            case R.id.navigation_order:
+                                openFragment(SmsFragment.newInstance("", ""));
+                                return true;
+                            case R.id.navigation_notifications:
+                                openFragment(NotificationFragment.newInstance("", ""));
+                                return true;
+                        }
+                        return false;
+                    }
+
+                };
+
+    }
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/alisha/takeawayapp/bottomNavItems/ProfileFragement.java
@@ -0,0 +1,4 @@
+package com.alisha.takeawayapp.bottomNavItems;
+
+public class ProfileFragement {
+}
