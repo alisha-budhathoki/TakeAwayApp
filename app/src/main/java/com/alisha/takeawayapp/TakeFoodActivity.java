f4d1423c23fdbcad7502e9a38068b97271c1791f
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2020-10-04 15:17:43 +0545

    adding navigation

---
+++ app/src/main/java/com/alisha/takeawayapp/TakeFoodActivity.java
@@ -0,0 +1,14 @@
+package com.alisha.takeawayapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TakeFoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_food);
+    }
+}
--- app/src/main/java/com/alisha/takeawayapp/bottomNavItems/HomeFragment.java
+++ app/src/main/java/com/alisha/takeawayapp/bottomNavItems/HomeFragment.java
@@ -1,21 +1,39 @@
 package com.alisha.takeawayapp.bottomNavItems;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.alisha.takeawayapp.R;
+import com.alisha.takeawayapp.TakeFoodActivity;
 
 public class HomeFragment extends Fragment {
+    FrameLayout takeFood, takeMart, takeSend;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragement_home, container, false);
+        takeFood = view.findViewById(R.id.menuTkFood);
+        takeMart = view.findViewById(R.id.menuTkMart);
+        takeSend = view.findViewById(R.id.menuTkSnd);
+
+        takeFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), TakeFoodActivity.class);
+//        intent.putExtra(CommonConstants.INTENT_EXTRA_MERCHANT_TYPE, CommonConstants.DEAL_TYPE_TAKE_FOOD);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
 }
