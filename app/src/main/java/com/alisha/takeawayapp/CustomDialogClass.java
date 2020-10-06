c0c7ee507cf1c8ea75714db73a6d6993aa66ed57
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2020-10-06 15:31:10 +0545

    add properties in dialog

--- app/src/main/java/com/alisha/takeawayapp/CustomDialogClass.java
+++ app/src/main/java/com/alisha/takeawayapp/CustomDialogClass.java
@@ -1,9 +1,12 @@
 package com.alisha.takeawayapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
@@ -13,6 +16,8 @@ public class CustomDialogClass extends Dialog {
     public Activity c;
     public Dialog d;
     public Button yes, no;
+    Button login;
+    Context context;
 
     public CustomDialogClass(Activity a) {
         super(a);
@@ -24,5 +29,14 @@ public class CustomDialogClass extends Dialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.popup_take_mart);
+        login = findViewById(R.id.btnLoginLL);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("sjs");
+                dismiss();
+            }
+        });
+
     }
 }
