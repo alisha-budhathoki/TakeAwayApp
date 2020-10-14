3996d3a28bf6e7abde18212f36b077cd8e3d6b5f
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2020-10-14 23:41:11 +0545

    Add another dialog

---
+++ app/src/main/java/com/alisha/takeawayapp/bottomNavItems/Confirm.java
@@ -0,0 +1,16 @@
+package com.alisha.takeawayapp.bottomNavItems;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.alisha.takeawayapp.R;
+
+public class Confirm extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm);
+    }
+}
--- app/src/main/java/com/alisha/takeawayapp/bottomNavItems/SettingFragment.java
+++ app/src/main/java/com/alisha/takeawayapp/bottomNavItems/SettingFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,10 +15,12 @@ import androidx.fragment.app.Fragment;
 
 import com.alisha.takeawayapp.R;
 import com.alisha.takeawayapp.TakeFoodActivity;
+import com.alisha.takeawayapp.TakeMartActivity;
 
 public class SettingFragment extends Fragment {
     TextView textView1, textView2;
     EditText editText;
+    Button sendButton;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -38,6 +41,22 @@ public class SettingFragment extends Fragment {
                 editText.setText("Food");
             }
         });
+        sendButton = view.findViewById(R.id.sendBtn);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Confirm.class);
+//        intent.putExtra(CommonConstants.INTENT_EXTRA_MERCHANT_TYPE, CommonConstants.DEAL_TYPE_TAKE_FOOD);
+                startActivity(intent);
+            }
+        });
+//        sendButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getActivity(), Confirm.class);
+//                startActivity(intent);
+//            }
+//        });
         return view;
     }
 }
