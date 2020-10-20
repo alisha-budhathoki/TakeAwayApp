e142495d5005f44f5fab2cde9313ad38b6629452
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2020-10-20 22:37:46 +0545

    Add copying text to clipboard on click feature

--- app/src/main/java/com/alisha/takeawayapp/CopyText.java
+++ app/src/main/java/com/alisha/takeawayapp/CopyText.java
@@ -2,6 +2,9 @@ package com.alisha.takeawayapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +13,7 @@ import android.widget.EditText;
 public class CopyText extends AppCompatActivity {
     EditText enteredText;
     EditText copiedText;
-    Button Copy;
+    Button Copy, copyClipboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +22,21 @@ public class CopyText extends AppCompatActivity {
         Copy = (Button) findViewById(R.id.button1);
         enteredText = (EditText) findViewById(R.id.editText1);
         copiedText = (EditText) findViewById(R.id.editText2);
+        copyClipboard = findViewById(R.id.copyClipBtn);
         Copy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 copiedText.setText(enteredText.getText().toString());
             }
-        });}}
+        });
+    copyClipboard.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            System.out.println("sdnksd");
+            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("label","Your Text");
+            clipboard.setPrimaryClip(clip);
+        }
+    });
+    }}
 
