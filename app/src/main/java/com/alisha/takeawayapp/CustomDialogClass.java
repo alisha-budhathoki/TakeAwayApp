dc2effa6b161d0cd5375693039c431a8581db55d
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2020-10-27 23:25:03 +0545

    add dialog and open camera and gallery for image upload

--- app/src/main/java/com/alisha/takeawayapp/CustomDialogClass.java
+++ app/src/main/java/com/alisha/takeawayapp/CustomDialogClass.java
@@ -5,23 +5,25 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import static android.app.Activity.RESULT_OK;
+
 public class CustomDialogClass extends Dialog {
     public Activity c;
     public Dialog d;
-    public Button yes, no;
-    Button login;
-    Context context;
-    ImageView crossImg;
-
+    TextView camera, gallery;
     public CustomDialogClass(Activity a) {
         super(a);
         // TODO Auto-generated constructor stub
@@ -33,17 +35,47 @@ public class CustomDialogClass extends Dialog {
     protected void onCreate(Bundle savedInstanceState) {
         System.out.println("dnsdsda");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.popup_take_mart);
-            crossImg = findViewById(R.id.img_cross);
+        setContentView(R.layout.pop_upload_pic);
 
 //        login = findViewById(R.id.btnLoginLL);
-        crossImg.setOnClickListener(new View.OnClickListener() {
+       camera = findViewById(R.id.tvCamera);
+        gallery = findViewById(R.id.tvGallery);
+        camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("sjs");
-                dismiss();
+                Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                c.startActivityForResult(takePicture, 0);//zero can be replaced with any action code (called requestCode)
+
+            }
+        });
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                c.startActivityForResult(pickPhoto , 1);//one can be replaced with any action code
             }
         });
 
     }
+//    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+//        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+//        switch(requestCode) {
+//            case 0:
+//                if(resultCode == RESULT_OK){
+//                    Uri selectedImage = imageReturnedIntent.getData();
+//                    imageview.setImageURI(selectedImage);
+//                }
+//
+//                break;
+//            case 1:
+//                if(resultCode == RESULT_OK){
+//                    Uri selectedImage = imageReturnedIntent.getData();
+//                    imageview.setImageURI(selectedImage);
+//                }
+//                break;
+//        }
+//    }
+
+
 }
--- app/src/main/java/com/alisha/takeawayapp/UploadPicture.java
+++ app/src/main/java/com/alisha/takeawayapp/UploadPicture.java
@@ -2,13 +2,33 @@ package com.alisha.takeawayapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 public class UploadPicture extends AppCompatActivity {
+    private static final int SELECTED_PIC = 1;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_picture);
+        imageView = (ImageView) findViewById(R.id.imageView);
     }
+    public void btnClick(View v) {
+        CustomDialogClass cdd = new CustomDialogClass(UploadPicture.this);
+//                cdd.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//                cdd.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                cdd.show();
+
+//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        startActivityForResult(intent, SELECTED_PIC);
+        System.out.println("njksajsj");
+    }
+
 }
